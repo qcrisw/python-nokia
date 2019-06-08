d929914cb92f685c9c8ac4a810b2e3afe127068b
Joe LaPenna <jlapenna@gmail.com>
2019-06-07 20:12:53 -0700

    Include client id in fetch_token

--- nokia/__init__.py
+++ nokia/__init__.py
@@ -83,7 +83,8 @@ class NokiaAuth(object):
         tokens = self._oauth().fetch_token(
             '%s/oauth2/token' % self.URL,
             code=code,
-            client_secret=self.consumer_secret)
+            client_secret=self.consumer_secret,
+            include_client_id=True)
 
         return NokiaCredentials(
             access_token=tokens['access_token'],
